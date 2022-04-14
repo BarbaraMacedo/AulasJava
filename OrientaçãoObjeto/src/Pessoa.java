ccefc02f52eb6e85dded8a8d9da0f7a1e248dafe
aluno <aluno@DSN1251017043>
2022-04-14 11:23:55 -0300

    Primeiro commit

--- OrientaçãoObjeto/src/Pessoa.java
+++ OrientaçãoObjeto/src/Pessoa.java
@@ -2,13 +2,25 @@
 public class Pessoa {
 	String nome;
 	int NumFigurinhas;
-	
+
 	void receber(int NumFigurinhas) {
 		this.NumFigurinhas += NumFigurinhas;
 	}
-	
-	void dar() {
-		
+
+	boolean dar(int NumFigurinhas, Pessoa pessoa) {
+		//boolean retorno;
+
+		if (this.NumFigurinhas<NumFigurinhas) {
+			System.out.println("Sem figurinhas suficientes!");
+			//retorno = false;
+			return false;
+		}
+		else {
+			this.NumFigurinhas -= NumFigurinhas;
+			pessoa.receber(NumFigurinhas);
+			//retorno = true;
+			return true;
+		}
+		//return retorno;
 	}
 }
-
--- OrientaçãoObjeto/src/main.java
+++ OrientaçãoObjeto/src/main.java
@@ -2,19 +2,35 @@
 public class main {
 
 	public static void main(String[] args) {
-		
+
 		Pessoa pessoa1 = new Pessoa();
-		
+
 		pessoa1.nome = "Bárbara";
 		pessoa1.NumFigurinhas = 1;
-		
+
 		pessoa1.receber(3);
-		
+
 		System.out.println(pessoa1.nome);
 		System.out.println(pessoa1.NumFigurinhas);
-		
-		
+
+		Pessoa pessoa2 = new Pessoa();
+
+		pessoa2.nome = "Gustavo";
+		pessoa2.NumFigurinhas = 3;
+
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.NumFigurinhas);
+
+		pessoa1.dar(5, pessoa2);
+
+		System.out.println("Depois");
+
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.NumFigurinhas);
+
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.NumFigurinhas);
+
 	}
 
-	
 }
