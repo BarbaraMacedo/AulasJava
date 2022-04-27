4fb61e0d7089001e0cc534b106717441a4b5ea39
aluno <aluno@DSN1251230705>
2022-04-27 11:46:11 -0300

    Segundo Commit

---
+++ ProjetoLampada/src/Lampada.java
@@ -0,0 +1,38 @@
+
+public class Lampada {
+	
+	private boolean ligada;
+	
+	
+	public void ligar() {
+	
+		ligada = true;
+		
+	}
+	
+	public void desligar() {
+	
+		ligada = false;
+		
+	}
+	
+	public void imprimir() {
+
+		if(ligada == true) {
+			System.out.println("Lampada ligada");
+			
+		}
+		else {
+			System.out.println("Lampada desligada");
+		}
+		
+	}
+
+	public Lampada(boolean ligada) {
+		
+		this.ligada = ligada;
+	}
+
+
+
+}
---
+++ ProjetoLampada/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Lampada lampada1 = new Lampada(false);
+	   
+		lampada1.imprimir();
+		lampada1.ligar();
+	
+		lampada1.desligar();
+	}
+
+}
