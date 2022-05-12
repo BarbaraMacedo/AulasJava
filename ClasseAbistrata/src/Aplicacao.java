167832a1232e0c916f6ebdf15975764380665d4d
Bárbara Macedo <barbara.bmslu@gmail.com>
2022-05-12 09:01:56 -0300

    Update Aplicacao.java

--- ClasseAbistrata/src/Aplicacao.java
+++ ClasseAbistrata/src/Aplicacao.java
@@ -3,7 +3,7 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		// Item item = new item();  Obs: N„o se pode inst‚nciar um objeto abstrato; / Classes abstratas n„o se pode instanciar
+		// Item item = new item();  Obs: N√£o se pode inst√¢nciar um objeto abstrato / Classes abstratas n√£o se pode instanciar
 		
 		Item item = new Cogumelo();
 		item.pegar();
