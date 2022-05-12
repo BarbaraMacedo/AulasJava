774fa6a18627e097f7bba0c302109afe0e4ed215
aluno <aluno@DSN1251017045>
2022-05-12 11:11:12 -0300

    Interfaces

---
+++ Interfaces/src/Aplicacao.java
@@ -0,0 +1,21 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		Rota rota = new rota();
+		Ferrari ferrari = new Ferrari();
+		rota.ir(ferrari);
+		System.out.println();
+		
+		Fusca fusca = new Fusca();
+		rota.ir(fusca);
+		
+		Vendedor vendedor = new Vendedor();
+		Apartamento apartamento = new Apartamento();
+		
+		vendedor.mostrarPreco(apartamento);
+		vendedor.mostrarPreco(ferrari);
+	}
+
+}
---
+++ Interfaces/src/Rota.java
@@ -0,0 +1,11 @@
+
+public class Rota {
+	
+	public void ir(Automovel automovel) {
+		automovel.virarEsquerda();
+		automovel.acelerar();
+		automovel.virarDireita();
+		automovel.virarDireita();
+	}
+
+}
