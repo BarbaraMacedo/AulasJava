c858197fc23076fa81535656d70efef44bfb08fa
Bárbara Macedo <barbara.bmslu@gmail.com>
2022-05-26 11:43:36 -0300

    Add files via upload

--- EscreverTxt/src/main.java
+++ EscreverTxt/src/main.java
@@ -1,18 +1,32 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
 	public static void main(String[] args) throws IOException {
-	
-		String s = "texto para ser gravado no arquivo";
-				
-		
-		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))){		
-			escrever.write(s);
-		}
-		
-	}
 
+	String s = "MOUSE";
+
+		Mouse mouse1 = new Mouse(true, 1, "pequeno");	
+		Mouse mouse2 = new Mouse(true, 2, "médio");	
+		Mouse mouse3 = new Mouse(true, 3, "grande");	
+	
+		List <Mouse> lista = new ArrayList <Mouse>();
+		
+		lista.add(mouse1);
+		lista.add(mouse2);
+		lista.add(mouse3);	
+		
+try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))){
+		
+			escrever.write(lista.toString());
+		}
+
+			for(Mouse mouse : lista) {	
+		System.out.println(lista);
+	}
+}
 }
