d24ad240c61ebb91ccf2d3f1281f240ba3f6d3ac
Bárbara Macedo <barbara.bmslu@gmail.com>
2022-05-27 14:24:37 -0300

    Update Constantes.java

--- ElementosStaticos/src/Constantes.java
+++ ElementosStaticos/src/Constantes.java
@@ -1,5 +1,5 @@
 
-public class Constantes {	
+public class Constantes {	// Sao valores que nao poderao ser alterados enquanto estiverem em execucao 
 	
 //	public static final double MEDIA_DA_PROVA = 7.0;
 	public static final double MEDIA_DA_PROVA;
