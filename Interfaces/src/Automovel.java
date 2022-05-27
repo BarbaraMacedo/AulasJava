64840a6107ea30c6ff90d3690dfeea8c6dc0a26d
Bárbara Macedo <barbara.bmslu@gmail.com>
2022-05-27 14:10:46 -0300

    Update Automovel.java

--- Interfaces/src/Automovel.java
+++ Interfaces/src/Automovel.java
@@ -3,7 +3,7 @@ public interface Automovel {
 	//Interface nao utiliza atributos
 	//Somente publica
 	
-	void virarEsquerda();   //assinatura
+	void virarEsquerda();   //assinatura do metodo
 	void virarDireita();
 	void acelerar();
 
