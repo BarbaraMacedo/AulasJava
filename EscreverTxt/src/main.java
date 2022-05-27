61ad9e33f3f19cdc274e64704f1f8c9ed3470c6a
Bárbara Macedo <barbara.bmslu@gmail.com>
2022-05-27 16:39:25 -0300

    Update main.java

--- EscreverTxt/src/main.java
+++ EscreverTxt/src/main.java
@@ -11,16 +11,16 @@ public class main {
 	String s = "MOUSE";
 
 		Mouse mouse1 = new Mouse(true, 1, "pequeno");	
-		Mouse mouse2 = new Mouse(true, 2, "médio");	
+		Mouse mouse2 = new Mouse(true, 2, "mÃ©dio");	
 		Mouse mouse3 = new Mouse(true, 3, "grande");	
 	
-		List <Mouse> lista = new ArrayList <Mouse>();
+		List <Mouse> lista = new ArrayList <Mouse>();  //Nova lista
 		
-		lista.add(mouse1);
+		lista.add(mouse1);  //Adicionando objetos na lista
 		lista.add(mouse2);
 		lista.add(mouse3);	
 		
-try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))){
+try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))){ //Utilizado para escrever em arquivo de texto
 		
 			escrever.write(lista.toString());
 		}
