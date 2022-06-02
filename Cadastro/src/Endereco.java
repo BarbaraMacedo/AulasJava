39d680b3f8f07749b1a32a9baec2cfa55022f3ca
aluno <aluno@DSN1251017045>
2022-06-02 11:31:53 -0300

    Projeto cadastro pessoas

--- Cadastro/src/Endereco.java
+++
@@ -1,9 +0,0 @@
-
-public class Endereco {
-	private int rua;
-	private String bairro;
-	private int num;
-	
-	
-	
-}
---
+++ Cadastro1/src/Aplicacao.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Aplicacao {
+
+	public static void main(String[] args) throws IOException {
+
+		Pessoa pessoa = new Pessoa("oi");
+
+		List<Pessoa> lista = new ArrayList<Pessoa>();
+
+		Scanner scanner = new Scanner(System.in);
+
+		Endereco endereco = new Endereco();
+
+		System.out.println("Digite o primeiro nome");
+		pessoa.setNome(scanner.nextLine());
+
+		System.out.println("Digite a idade");
+		scanner.nextInt();
+
+		System.out.println("Digite o bairro");
+		scanner.next();
+
+		System.out.println("Digite a rua");
+		scanner.next();
+
+		System.out.println("Digite o número");
+		scanner.nextInt();
+
+		System.out.println("Sexo:");
+		System.out.println("1 = Feminino  2 = Masculino");
+
+		int x = 0;
+		while (x != 1 && x != 2) {
+			x = scanner.nextInt();
+			if (x == 2)
+				pessoa.setSexo(Sexo.MASCULINO);
+
+			else if (x == 1)
+				pessoa.setSexo(Sexo.FEMININO);
+			else {
+				System.out.println("Opção não válida");
+				System.out.println("1 = Feminino  2 = Masculino");
+			}
+		}
+
+		lista.add(pessoa);
+
+		try (BufferedWriter escrever = new BufferedWriter(new FileWriter("gravar.txt"))) {
+			for (Pessoa pessoa1 : lista)
+				escrever.write(pessoa1.toString() + "\n");
+		}
+		try (BufferedReader reader = new BufferedReader(new FileReader("gravar.txt"))) {
+			String line;
+			String string = "";
+
+			while ((line = reader.readLine()) != null) {
+
+		}
+			
+			System.out.println(scanner);
+		}
+
+	}
+}
